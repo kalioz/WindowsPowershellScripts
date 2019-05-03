9ff49580ec55af29e6de6f9d666f25c7e35aa64d
LOISELET <clement.loiselet@capgemini.com>
2019-05-03 15:21:15 +0200

    added README

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,13 +4,13 @@ Get-ChildItem "${psdir}\*.ps1" | %{.$_}
 
 ### useful vars
 $bashrc='export PS1="\u@\h:\w\\$ "'
-Function bashrc {
-    echo $bashrc
-}
-
-
 $gitlab="~\Documents\gitlab"
 $github="~\Documents\github"
+$shellStartupDate=$(Get-Date -UFormat "%A %d %B %T")
 
+Function bashrc { echo $bashrc }
 Function gitlab { cd $gitlab }
 Function github { cd $github }
+Function startupDate { echo $shellStartupDate}
+
+Echo "Console loaded at $shellStartupDate"
