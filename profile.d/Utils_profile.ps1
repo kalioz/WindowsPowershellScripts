7e80d5ffdb2e52b34742667dde9f0e316f34cf9e
Clement LOISELET <clement.loiselet@capgemini.com>
2019-07-31 17:17:50 +0200

    updated prompt

--- profile.d/Utils_profile.ps1
+++ profile.d/Utils_profile.ps1
@@ -2,36 +2,95 @@
 
 ### prompt
 function prompt {
-    "PS $(Get-Date -UFormat '%R') $(get-location)>"
+  # colors
+  $ESC= [char]27
+  $red = "$ESC[91m"
+  $green = "$ESC[92m"
+  $red_2 = "$ESC[31m"
+  $green_2 = "$ESC[32m"
+  $reset = "$ESC[0m"
+  
+  # arrows
+  $up_arrow=[char]8593
+  $down_arrow=[char]8595
+  
+  # if a command was executed before : print time of execution
+  $blacklist=@("git","cd","ls")
+  if ((history).length -gt 0 -and $False){
+    $last_command = (history)[-1]
+    $blacklisted = $False
+    ForEach ($black in $blacklist){
+      if ($last_command.CommandLine.StartsWith($black)){
+        $blacklisted = $True
+        break
+      }
+    }
+    if (! $blacklisted){
+      echo "$($last_command.StartExecutionTime.ToString('HH:mm:ss')) -> $($last_command.EndExecutionTime.ToString('HH:mm:ss')) ($([math]::Round(($last_command.EndExecutionTime - $last_command.StartExecutionTime).TotalSeconds, 3))s)"
+    }
+  }
+
+  # date
+  $output="${reset}PS $(Get-Date -UFormat '%R')"
+  
+  # location
+  $location = $(get-location).Path
+  if ($location.length -gt "$HOME/".length){
+    $location = $location.replace("$HOME",'~')
+  }
+  if ( $location.Contains(" ") ){
+    $output+=" `"$location`" "
+  } else {
+    $output+=" $location "
+  }
+  
+  # git branch
+  $G_BRANCH = git branch -vv 2>$null | Select-String -Pattern "\* (?<name>[^ ]+).*" 
+  if ($?) {
+    # fetch last updates if not done in more than 30 minutes
+    $FETCH_HEAD="$(git rev-parse --show-toplevel)/.git/FETCH_HEAD"
+    if (Get-Item $FETCH_HEAD | Select -Property LastWriteTime | %{$_.LastWriteTime.AddMinutes(120) -lt (Get-Date)}){
+      git fetch > $null
+      $G_BRANCH = git branch -vv 2>$null | Select-String -Pattern "\* (?<name>[^ ]+).*"
+    }
+    
+    $current_branch = $G_BRANCH.matches.groups[1].value
+    # find difference between current and up
+    $ahead_match = $G_BRANCH.Line | Select-String -Pattern "ahead ([0-9]+)"
+    $ahead=""
+    if ($ahead_match.matches.groups.length -eq 2){
+      $a=$ahead_match.matches.groups[1].value
+      $ahead="${a}${up_arrow}"
+    }
+    
+    $behind_match = $G_BRANCH.Line | Select-String -Pattern "behind ([0-9]+)"
+    $behind=""
+    if ($behind_match.matches.groups.length -eq 2){
+      $b=$behind_match.matches.groups[1].value
+      $behind="${b}${down_arrow}"
+    }
+    
+    $gitdiff=""
+    if ($behind.length -gt 0) {
+      $gitdiff+= " ${red_2}${behind}${reset}"
+    }
+    if ($ahead.length -gt 0) {
+      $gitdiff+= " ${green_2}${ahead}${reset}"
+    }
+    
+    $default_branch = git symbolic-ref refs/remotes/origin/HEAD | %{$_ -replace "refs/remotes/origin/",""}
+    if ("$default_branch" -eq "$current_branch") {
+      $output+="[${red}${current_branch}${reset}${gitdiff}] "
+    } else {
+      $output+="[${green}${current_branch}${reset}${gitdiff}] "
+    }
+  }
+  
+  # end char
+  $output+="$ "
+  
+  echo $output
 }
-
-### cd - Change Directory
-
-#### cdl = cd+ls
-Function cdl {
-	cd_unix $args[0]
-	get-ChildItem
-}
-
-
-#### cdd = cd+ls ..
-Function cdd {
-	Set-Location ..
-	get-ChildItem
-}
-
-#### cd_unix = cd <path> normal, cd default to home directory (accept spaces in name, cd path/to my/file)
-Function cd_unix {
-	if ( $args[0] ){
-		Set-Location "$args"
-	}else{
-		$c = Read-Host "press enter to go to home"
-		Set-Location ~/
-	}
-}
-
-del alias:cd -Force
-Set-Alias cd cd_unix
 
 ### ssh tunnel using plink (putty headless)
 Function sshtunnel {
@@ -47,36 +106,13 @@ Function sshtunnel {
 	plink.exe -ssh ${reboundUser}@${reboundserver} -i $reboundKey -L ${localPort}:${mysqlserver}:${mysqlserverport} -N
 }
 
-### ssh using bash (WSL)
-Function ssh {
-    bash -c "ssh $args"
-}
-
-### touch function
-Function touch {
-    foreach ($path in $args){
-        $null >> $path
-    }
-}
-
-### Where function (Where-Object doesn't act as bash:where)
-del alias:where -Force
-
-### Curl function (Invoke-WebRequest jsut doesn't do it justice)
-del alias:curl -Force
-
-Function curl {
-  bash -c "curl $args"
-}
-
-### printenv
-Function printenv {
-  Get-ChildItem Env:
-}
 
 ### JMeter 
 Function jmeter2 {
-  Start-Job {jmeter $args}
+  Start-Job {
+    Set-Location /tmp
+    jmeter $args
+  }
 }
 
 ### Utils
@@ -90,38 +126,66 @@ Function ip(){
     if (! $args[0]){
         # no args : display all active connexions
         #virtual first
-        echo "Virtual"
-        echo "-------"
+        Write-Output "Virtual"
+        Write-Output "-------"
         Foreach($interface in $a){
             if ($interface.AddressState -like "*Preferred*" -and $interface.AddressFamily -like "*IPv4*" -and ($interface.InterfaceAlias -like "*VEthernet*" -or $interface.InterfaceAlias -like "*Virtual*")){
-                echo "$($interface.InterfaceAlias) : $($interface.IPAddress)"
+                Write-Output "$($interface.InterfaceAlias) : $($interface.IPAddress)"
             }
         }
-        echo ""
+        Write-Output ""
         #real after
-        echo "Real"
-        echo "----"
+        Write-Output "Real"
+        Write-Output "----"
         Foreach($interface in $a){
             if ($interface.AddressState -like "*Preferred*" -and $interface.AddressFamily -like "*IPv4*" -and !($interface.InterfaceAlias -like "*VEthernet*" -or $interface.InterfaceAlias -like "*Virtual*")){
-                echo "$($interface.InterfaceAlias) : $($interface.IPAddress)"
+                Write-Output "$($interface.InterfaceAlias) : $($interface.IPAddress)"
             }
         }
-        echo ""
-        echo "External"
-        echo "--------"
-        echo "WebIp : $((curl 'https://api.ipify.org').Content)"
     }else{
         Foreach($interface in $a){
             if ($interface.AddressFamily -like "*IPv4*" -and $interface.InterfaceAlias -like "$($args[0])*"){
-                echo "$($interface.InterfaceAlias) : $($interface.IPAddress) ($($interface.AddressState))"
+                Write-Output "$($interface.InterfaceAlias) : $($interface.IPAddress) ($($interface.AddressState))"
             }
         }
     }
+    Write-Output ""
+    Write-Output "External"
+    Write-Output "--------"
+    Write-Output "WebIp : $((Invoke-WebRequest 'https://api.ipify.org').Content)"
 }
 
 Function pwn_cisco {
-	$c = Read-Host "This command will stop all cisco services. are you sure you want to proceed ? (y/n): "
-	if (($c -eq 'y') -or ($c -eq 'Y') -or ($c -eq 'yes')){
+	$c = Get-User-Permission "This command will stop all cisco services. are you sure you want to proceed ? (y/n): "
+	if ($c){
 		Get-Service -DisplayName "*cisco*" | Stop-Service
 	}
 }
+
+Function pwn_symantec {
+	$c = Get-User-Permission "This command will stop all symantec services. are you sure you want to proceed ? (y/n): "
+	if ($c){
+		Get-Service -DisplayName "*symantec*" | Stop-Service
+	}
+}
+
+Function Get-User-Permission {
+    <#
+    .DESCRIPTION
+    asks for user confirmation and return a boolean.
+
+    .PARAMETER message
+    The message to display to the user. (default = "confirm ?")
+    #>
+
+    param (
+        [string] $message = "confirm ? "
+    )
+
+    $c = Read-Host $message
+
+    return ($c -eq 'y') -or ($c -eq 'Y') -or ($c -eq 'yes') -or ($c -eq 'o') -or ($c -eq 'O') -or ($c -eq 'oui')
+}
+
+Set-Alias nano notepad++.exe
+Set-Alias vi notepad++.exe
