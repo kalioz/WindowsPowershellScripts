584ad5a8ab112c676c9b3bc4577db87b3df9d5e6
Clement LOISELET <clement.loiselet@capgemini.com>
2019-07-31 17:16:03 +0200

    added some functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,24 @@
 # Load all subdirectories
 $psdir = "~\Documents\WindowsPowerShell\profile.d"
-Get-ChildItem "${psdir}\*.ps1" | %{.$_} 
+Get-ChildItem "${psdir}\*.ps1" | ForEach-Object{.$_} 
 
 ### useful vars
 $bashrc='export PS1="\u@\h:\w\\$ "'
 $gitlab="~\Documents\gitlab"
+$devops="$gitlab\devops"
+$application="$gitlab\application"
+
 $github="~\Documents\github"
 $shellStartupDate=$(Get-Date -UFormat "%A %d %B %T")
 
-Function bashrc { echo $bashrc }
-Function gitlab { cd $gitlab }
-Function github { cd $github }
-Function startupDate { echo $shellStartupDate}
+Function bashrc { 
+  Set-Clipboard -value $bashrc
+  Write-Output $bashrc 
+}
+Function gitlab { Set-Location $gitlab }
+Function devops { Set-Location $devops }
+Function application { Set-Location $application }
+Function github { Set-Location $github }
+Function startupDate { Write-Output $shellStartupDate}
 
-Echo "Console loaded at $shellStartupDate"
+Write-Output "Console loaded at $shellStartupDate"
